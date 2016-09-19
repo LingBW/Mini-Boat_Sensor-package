4eeebf057530263d30b9e2815c694e4c321663cb
Bingwei Ling <ufwrgw@gmail.com>
2016-09-19 09:34:06 -0400

    Update temp_DS18B20-multiple.py

--- temp_DS18B20-multiple.py
+++ temp_DS18B20-multiple.py
@@ -1,108 +1,133 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 25 10:50:18 2016
-
-@author: bling
-"""
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec 31 09:32:13 2015
-@author: bling
+@author: Bingwei Ling
 """
 
 import os, glob, time
 import serial,numpy
+import datetime
  
+# Hard codes
+transmit = 'ON' # ON,OFF
+send_interval = 7100 # unit: seconds. eg 2*3600
+temp_inter = 58 #collect data interval, every minute
+sd_num = 3 #send data times
+
+cdatas = [[],[],[],[],[]] #celcius degree lists
+
 os.system('sudo modprobe w1-gpio')
 os.system('sudo modprobe w1-therm')
- 
-base_dir = '/sys/bus/w1/devices/'
-#device_folder = glob.glob(base_dir + '28*')[0]
-device_folder = glob.glob(base_dir + '28*')
-device_files = []
-for i in device_folder:
-    device_file = i + '/w1_slave'
-    device_files.append(device_file)
-    
 
-transmit = 'ON' # ON,OFF
-cs1 = []; cs2 = [] #celcius degree lists
-send_interval = 5 # unit: minute
- 
-def read_temp_raw():
-    lines = []
-    for i in device_files:
-        f = open(i, 'r')
-        lines.append(f.readlines())
-        f.close()
+def read_temp_raw(device_file):
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+
     return lines
  
 def read_temp():
-    temp_c = []
-    #temp_f = []
-    lines = read_temp_raw()
-    for i in lines:
-        '''while i[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = read_temp_raw() #'''
-        if i[0].strip()[-3:] != 'YES':
+    dic_dates = {}
+    device_folder = glob.glob('/sys/bus/w1/devices/28*')
+    # If no such files, return None, and , send message '00000'
+    if not device_folder:
+        return None
+    
+    for i in device_folder:
+        key = i[-12:]
+        device_file = i + '/w1_slave'
+        try:
+            lines = read_temp_raw(device_file)
+        except:
+            dic_dates[key] = 0
             continue
-        equals_pos = i[1].find('t=')
+        if lines[0].strip()[-3:] != 'YES':
+            dic_dates[key] = 0
+            continue
+        equals_pos = lines[1].find('t=')
         if equals_pos != -1:
-            temp_string = i[1][equals_pos+2:]
-            temp_c.append((float(temp_string)/1000.0+30)*100)
-            #temp_f.append(float(temp_string)/1000.0 * 9.0 / 5.0 + 32.0)
-    return temp_c#, temp_f
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0 + 30  # avoid negative value.
+            #temp_f = temp_c * 9.0 / 5.0 + 32.0
+            if temp_c>0 and temp_c<100:
+                dic_dates[key] = temp_c*10
+            else :
+                dic_dates[key] = 0
     
-time.sleep(3)	
-while True:
-    #cs,fs = read_temp()
-    cs = read_temp()
-    print "temp1: %d, temp2: %d" % (cs[0],cs[1])
-    cs1.append(cs[0]); cs2.append(cs[1])
-    
-    temp_inter = 60 #collect data interval, every minute
-    
-    if len(cs1) == send_interval:
-        mes10 = numpy.mean(cs1); mes20 = numpy.mean(cs2)
-        mes11 = numpy.min(cs1); mes21 = numpy.min(cs2)
-        mes12 = numpy.max(cs1); mes22 = numpy.max(cs2)
+    return dic_dates #, temp_f #return a list of each temperature-sensor value.
 
-        mes10 = str(int(mes10)); mes20 = str(int(mes20))
-        mes11 = str(int(mes11)); mes21 = str(int(mes21))
-        mes12 = str(int(mes12)); mes22 = str(int(mes22))
-        
-        mes = mes10+mes11+mes12 + mes20+mes21+mes22      
+def transdata(mes):
+    try:
+        try:
+            ser=serial.Serial('/dev/ttyUSB0',9600) # linux
+        except:
+            ser=serial.Serial('/dev/ttyUSB1',9600)
+        # send the data
+        time.sleep(1)
+        ser.writelines('\n')       
+        time.sleep(1)
+        ser.writelines('\n')
+        time.sleep(1)
+        ser.writelines('yab'+'\n') # Force the given message to idle.
+        time.sleep(5)
+        ser.writelines('\n')
+        time.sleep(1)
+        ser.writelines('\n')
+        time.sleep(1)
+        ser.writelines('ylb'+mes+'\n')
+        time.sleep(1) # 1100s 18 minutes        
+        ser.close() # close port
+        time.sleep(1)
+    except:
+        print 'Can not send data.'  #'''
+        return 0
+    return 1
+    
+ki = 0 # count failsure times.
+kp = 0 # Halt Pi
+time.sleep(3)	
+sendtime = datetime.datetime.now()
+while True:
+    looptime = datetime.datetime.now()
+    cs = read_temp()
+    #print looptime,cs
+    
+    # Loops for no temp-sensors.
+    if not cs:
+        time.sleep(0.2)
+        print ki
+        if ki == 100:
+            nmes = '00000'
+            transdata(nmes)
+            ki = 0
+            time.sleep(3600)
+        ki = ki+1
+        continue
+    
+    for j in range(len(cs)):
+        cdatas[j].append(cs.values()[j])
+
+    if (looptime-sendtime).total_seconds() >= send_interval:
+        sendtime = datetime.datetime.now()
+        mes = ''
+        #print cdatas
+        for b in cdatas:
+            if len(b) != 0:
+                mes0 = numpy.mean(b)
+                mes1 = numpy.min(b)
+                mes2 = numpy.max(b)
+                mes3 = str(int(mes0))+str(int(mes1))+str(int(mes2))
+                if mes3 == '000':
+                    mes3 = '111111111'#'''
+                mes = mes + mes3
+
+        for k in cdatas:
+            del k[:]
         print mes
-        
         if transmit == 'ON':
-            try:
-                #ser=serial.Serial('COM16', 9600) # in Windows
-                ser=serial.Serial('/dev/ttyUSB0',9600) # linux
-                #print 'Open serial port.'
-                # send the data
-                time.sleep(1)
-                ser.writelines('\n')       
-                time.sleep(1)
-                ser.writelines('\n')
-                time.sleep(1)
-                ser.writelines('yab'+'\n') # Force the given message to idle.
-                time.sleep(5)
-                ser.writelines('\n')
-                time.sleep(1)
-                ser.writelines('\n')
-                time.sleep(1)
-                #ser.writelines('ylb9'+meandepth+rangedepth+time_len+meantemp+sdeviatemp+'\n')
-                ser.writelines('ylb00'+mes+'\n')
-                #time.sleep(3)
-                #print 'Sending data: '+meandepth+rangedepth+time_len+meantemp+sdeviatemp
-                #print datetime.now()
-                time.sleep(2) # 1100s 18 minutes        
-                ser.close() # close port
-                #time.sleep(288)
-                temp_inter = temp_inter-12
-            except:
-                print 'Can not send data.'  #'''
+            transdata(mes)
+        kp = kp+1
+    if kp == sd_num:
+        os.system('sudo halt')
     time.sleep(temp_inter)
